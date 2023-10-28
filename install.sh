114e2ba8f6a3beb4726e83f874e32441b1c9b892
SoaB1 <kumachan1230@gmail.com>
2023-10-29 02:08:00 +0900

    build: インストールスクリプトを更新

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# Install git
-add-apt-repository ppa:git-core/ppa
+# Install dependencies
+apt-get install -y software-properties-common sshpass
+
+# Add Ansible and Git PPA
+add-apt-repository --yes --update ppa:ansible/ansible
+add-apt-repository --yes --update ppa:git-core/ppa
 apt-get update
+
+# Install git
 apt-get install -y git
+
+# Install Ansible
+apt-get install -y ansible
